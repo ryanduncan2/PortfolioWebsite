e8ae4a4c9c78e2b099f73505d0ef872f3298abc3
Sparky <ryanduncan114@gmail.com>
2024-09-16 16:16:22 +1000

    Added support for HTTPS.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+import fs from 'fs';
+import http from 'http';
+import https from 'https'
 import express from 'express';
 import cors from 'cors';
+
+var privateKey = fs.readFileSync('privatekey.key', 'utf8');
+var certificate = fs.readFileSync('certificate.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 
 app.use(cors());
 app.use(express.static('public'));
 
-app.listen(80, () =>
-{
-    console.log('Listening on Port 80');
-});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
