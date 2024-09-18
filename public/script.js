402fb337e8aea8d4425d9cda076b38d9985a10aa
Sparky <ryanduncan114@gmail.com>
2024-09-19 07:33:08 +1000

    Populated projects page with information.

---
+++ public/script.js
@@ -0,0 +1,23 @@
+window.onload = function() 
+{
+    document.getElementById("password-manager-button").onclick = function () 
+    {
+        location.href = "https://github.com/ryanduncan2/PasswordManager";
+    };
+    
+    document.getElementById("crazy-engine-button").onclick = function () 
+    {
+        location.href = "https://github.com/ryanduncan2/CrazyEngine";
+    };
+    
+    document.getElementById("math-script-button").onclick = function () 
+    {
+        location.href = "https://github.com/ryanduncan2/CrazyEngine";
+    };
+    
+    document.getElementById("crazy-engine-gui-button").onclick = function () 
+    {
+        location.href = "https://github.com/ryanduncan2/CrazyEngine";
+    };
+};
+
