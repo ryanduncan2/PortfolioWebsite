81c009ea2970b2ee092bb3f487293d13fd125d08
Sparky <ryanduncan114@gmail.com>
2024-09-21 06:26:06 +1000

    Added icon and removed .html extensions.

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-import fs from 'fs';
-import http from 'http';
-import https from 'https'
+// import fs from 'fs';
+// import http from 'http';
+// import https from 'https'
 import express from 'express';
 import cors from 'cors';
 
-var privateKey = fs.readFileSync('privatekey.key', 'utf8');
-var certificate = fs.readFileSync('certificate.crt', 'utf8');
-var credentials = { key: privateKey, cert: certificate };
+// var privateKey = fs.readFileSync('privatekey.key', 'utf8');
+// var certificate = fs.readFileSync('certificate.crt', 'utf8');
+// var credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('public', { extensions:['html'] }));
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+app.listen(8080, () =>
+{
+    console.log("Listening on port 8080.");
+});
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(8080);
+// httpsServer.listen(8443);
